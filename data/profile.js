c666f29e28f16f7f49a4a84e5a10cba3baef249f
Mrrob25 <robin.mishra81@gmail.com>
2021-07-23 11:20:15 -0700

    adding new files

---
+++ data/profile.js
@@ -0,0 +1,100 @@
+const profileData = {
+    "title": "Profile",
+    "name": "Rabin Mishra",
+    "sub_title": "B. Tech undergraduate (Electrical Engineering)",
+    "about": {
+        "intro": "I am a creative , disciplined , dedicated tech- savvy , i likes to engage with new technologies which are combinations of software and electronics like Internet of Things , Robotics , AI and Computer Systems along with coding. I have also reasonable knowledge in Internet of Things and huge knowledge in Programming, Data Structures and Algorithms. I mostly used to stick with internet and computer systems.For refreshment you see me playing with kids,watching web/TV series,playing mobile games.",
+        "contact": {
+             "email": "robin.mishra81@gmail.com",
+            "phone": "+919773770113",
+            "address": "MAQSOOD HOSTEL,ARSHA LIBRARY,HAUZ KHAS ,NEW DELHI-110016",
+            "portfolio": "https://mrrob25.github.io/resume/"
+        }
+    },
+    "links": [
+        { "title": "GitHub", "src": "https://github.com/mrrob25" },
+        { "title": "freeCodeCamp", "src": "https://www.freecodecamp.org/mrrob25" },
+        { "title": "HackerRank", "src": "https://www.hackerrank.com/mrrob25" },  
+	{ "title": "Twitter", "src": "https://twitter.com/mrrob25" },  
+        { "title": "LinkedIn", "src": "https://www.linkedin.com/in/mrrob25" }
+    ],
+    "education": [ 
+        {
+            "alma": "Faculty of Engineering and Technology, GKV, Haridwar",
+            "duration": "2014 - 2018", "std": "B. Tech (EE)",
+            "score": "65.00%"
+        },
+        {
+            "alma": "SSS Inter College , Rohaniya, Varanasi, Uttar Pradesh",
+            "duration": "2010 - 2012", "std": "Class XI - XII",
+            "score": "80.40%"
+        },
+        {
+            "alma": "SSS Inter College , Rohaniya, Varanasi, Uttar Pradesh",
+            "duration": "2008 - 2011", "std": "Class IX - X",
+            "score": "78.00%"
+        }
+    ],
+    "skills": [
+        {
+            "graph": "false",
+            "category": "Programming Languages",
+            "topics": [
+                "C","JS"
+            ]
+        },
+        {
+            "graph": "false",
+            "category": "Scripting-Database",
+            "topics": [
+                "HTML", "CSS", "Git/Github", "MySQL"
+            ]
+        }
+    ],
+    "projects": [{
+            	"title": "Ivisible Broken Wire Detection System",
+		"duration": "January,2018  - April,2018",
+            	"link": "https://github.com/mrrob25/finalprojectg.git",
+            	"desc": "In this project, I have created a system that detects the invsible broken wire "
+        	}
+		
+    ],
+    "experiences": [
+	{
+            "desc": "<b>Head Organizer </b> of a event at  <a target='_blank' href='https://www.jnanagni.in'> <strong> Jnanagni'17 </strong> </a>",
+            "date": "March 2017",
+            "details": [
+                "Organised the event Extempore and Debate.",
+                "Leaded the organisation team."
+            ]
+        }
+    ],
+    "events": [
+        {
+            "title": "certifications",
+            "body": [
+		{
+                    "desc": "a online learning course in <strong> JavaScript Essential Training </strong> hosted by <b> Linkedin </b>",
+                    "date": "18:July, 2021"
+                },
+		{
+                    "desc": "a training program in <strong> Python </strong> conducted by <b>IIT Bombay</b>funded by<b>MHRD,India Government</b>",
+                    "date": "December, 2017"
+                }
+            ]
+        },
+        {
+            "title": "other",
+            "body": [
+		 {
+                     "desc": "Winner of university cricket league <strong> Gurulul Premier League </strong> ",
+                     "date": "March 2017"
+                },
+	        {
+                     "desc": "Secured <strong> Rank 14100</strong> in <b> Gate CS 2020</b> with <b> Score 350</b>",
+                     "date": "March 2020"
+                }
+            ]
+        }
+    ]
+}
